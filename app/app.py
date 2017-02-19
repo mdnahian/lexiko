b0bc09ccebdbbac84a69cea27e8f1b27fad283a9
root <root@ip-172-31-25-237.ec2.internal>
2017-02-19 16:14:16 +0000

    second

--- app/app.py
+++ app/app.py
@@ -7,13 +7,17 @@ import random
 import io
 import httplib, urllib, base64
 import json
+import re
+from  webMD import *
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'lexiko'
 socketio = SocketIO(app)
 
 
-BASE_URL = 'http://ec2-174-129-79-187.compute-1.amazonaws.com'
+BASE_URL = 'http://ec2-174-129-79-187.compute-1.amazonaws.com'	
+	
+
 
 
 def image_to_text(url):
@@ -34,12 +38,12 @@ def image_to_text(url):
 	    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
 	    conn.request("POST", "/vision/v1.0/ocr?%s" % params, '{ "url":"'+url+'" }', headers)
 	    response = conn.getresponse()
-	    data = response.read()
-	    print data 
+	    data = response.read() 
 	    conn.close()
+	    return data
 	except Exception as e:
 	    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
+	    return ''
 
 
 
@@ -58,8 +62,33 @@ def image_sent(raw):
 
 	imgg = BASE_URL+'/img/'+fileName+'.jpg'
 
-	image_to_text(imgg)
+	raw_text = image_to_text(imgg)
+		
 
+	if raw_text != '':
+		
+		data = json.loads(raw_text)
+		
+		regions = data['regions']
+	    	for region in regions:
+	    		for line in region['lines']:
+				for word in line['words']:
+					text = ''.join(i for i in word['text'] if not i.isdigit())
+					text = re.sub('\W+', ' ', text)
+
+					if len(text) >= 5:
+						print text
+						response = text #search_web_md(text)
+						
+						if response != '':
+							emit('response', '{ "name":"something" }')
+							break
+
+		return ''						
+		
+	else:
+		print 'could not find any text in image'
+	
 
 
 
@@ -70,5 +99,5 @@ def sound_file(path):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(80))
+    socketio.run(app, host='0.0.0.0', port=int(80), debug=True)
 	
